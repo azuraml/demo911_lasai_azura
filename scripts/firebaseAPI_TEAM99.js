4d704d9fcdbfec9b32304d13a0aec7d713a7263c
Azura Rocks <lasai.azura.maya@gmail.com>
2023-03-07 08:18:54 -0800

    hi

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,7 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    //keys go here 
+    apiKey: "AIzaSyAYqk752vSfIug2L2jsbv4rB90_R5A2Fpw",
+    authDomain: "comp1800-d17fe.firebaseapp.com",
+    projectId: "comp1800-d17fe",
+    storageBucket: "comp1800-d17fe.appspot.com",
+    messagingSenderId: "968740684965",
+    appId: "1:968740684965:web:c54cce947699e1cd36b94c"
   
 };
 
